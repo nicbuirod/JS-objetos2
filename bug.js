c2e48c74aed06bb4a4f94d99524d27dc203cf042
Nicolas <nicbuirod@hotmail.com>
2022-11-23 19:37:48 -0500

    mejora de codigo

--- bug.js
+++ bug.js
@@ -1,3 +1,5 @@
+
+//solución 1
 let checkEquals = (arr1, arr2) =>{
   let res = false;
   for (let i = 0; i < arr1.length; i++) {
@@ -16,3 +18,15 @@ console.log(checkEquals([1, 2], [1, 2]));
 console.log(checkEquals([4, 5, 6], [4, 5, 6]));
 console.log(checkEquals([4, 7, 6], [4, 5, 6]));
 console.log(checkEquals([4, 7, 6], [4, 6, 7]));
+
+//solución 2
+const checkEquals2 = (arr1, arr2) => {  
+    
+   return arr1.toString() === arr2.toString() 
+}
+
+console.log(checkEquals2([1, 2], [1, 3]));
+console.log(checkEquals2([1, 2], [1, 2]));
+console.log(checkEquals2([4, 5, 6], [4, 5, 6]));
+console.log(checkEquals2([4, 7, 6], [4, 5, 6]));
+console.log(checkEquals2([4, 7, 6], [4, 6, 7]));
